33835ae51c8c98bb8dbfb80b13c73401d4476cb8
vchhieng <vchhieng@ebay.com.au>
2015-06-18 17:17:08 +1000

    cleanup pom

--- research/src/test/java/benchmark/Issue011ShoutdownTrickWithHaltTimeoutTest.java
+++ research/src/test/java/benchmark/Issue011ShoutdownTrickWithHaltTimeoutTest.java
@@ -9,7 +9,7 @@ import org.junit.runners.JUnit4;
  */
 @RunWith(JUnit4.class)
 public class Issue011ShoutdownTrickWithHaltTimeoutTest {
-    @Test
+//    @Test
     public void shouldNotShutdown() {
         Runtime.getRuntime().addShutdownHook(new Thread() {
             public void run() {
