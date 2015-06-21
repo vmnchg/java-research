151f83ab61f63bd4d70a578070d3d57f737933bb
vchhieng <vchhieng@ebay.com.au>
2015-06-22 09:55:14 +1000

    demoable code.

--- annotation/src/test/java/annotation/NoArgsConstructorProcessorTest.java
+++ annotation/src/test/java/annotation/NoArgsConstructorProcessorTest.java
@@ -29,8 +29,8 @@ public class NoArgsConstructorProcessorTest {
         public SeveralConstructors(int ai) {
         }
 
-    /*    public SeveralConstructors() {
-        }*/
+        public SeveralConstructors() {
+        }
     }
 
     // Fails
