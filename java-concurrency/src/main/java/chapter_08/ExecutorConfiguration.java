49165283a9ab0f14dc55ddf63f4fa5005cf6de5e
Van Munin Chhieng <v.m.chhieng@gmail.com>
2015-07-08 17:29:34 +1000

    adding timing task.

--- java-concurrency/src/main/java/chapter_08/ExecutorConfiguration.java
+++ java-concurrency/src/main/java/chapter_08/ExecutorConfiguration.java
@@ -1,12 +1,7 @@
 package chapter_08;
 
-import java.util.LinkedList;
-import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
@@ -24,14 +19,14 @@ public class ExecutorConfiguration {
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
     }
 
-    public Callable<Integer> makeCallable(final int runningTime) {
+    public Callable<Integer> makeCallable(final int runningTime, final int sleepingTime) {
         Callable<Integer> callable = new Callable<Integer>() {
             @Override
             public Integer call() throws Exception {
                 // TODO Auto-generated method stub
                 for (int i = 0; i < runningTime; i++) {
-                    Thread.sleep(TimeUnit.SECONDS.toSeconds(1000));
-                    System.out.print(".");
+                    Thread.sleep(TimeUnit.SECONDS.toSeconds(sleepingTime));
+                    System.out.println(Thread.currentThread().toString() + ".");
                 }
                 System.out.println();
                 return new Integer(runningTime);
@@ -81,37 +76,5 @@ public class ExecutorConfiguration {
                 super.terminated();
             }
         }
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-        BlockingQueue<Runnable> blockingQueue = new LinkedBlockingQueue<Runnable>();
-        ExecutorConfiguration executor = new ExecutorConfiguration(10, 10, 2, TimeUnit.SECONDS, blockingQueue);
-
-        List<Callable<Integer>> tasks = new LinkedList<Callable<Integer>>();
-        int numberOfCallable = 10;
-        for (int i = 1; i <= numberOfCallable; i++) {
-            tasks.add(executor.makeCallable(i));
-        }
-
-        List<Future<Integer>> fs = executor.executor.invokeAll(tasks);
-
-        System.out.println("<Done>");
-        for (int i = 0; i < numberOfCallable; i++) {
-            Future<Integer> f = fs.get(i);
-            Integer integer = null;
-            try {
-                integer = f.get();
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (ExecutionException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } finally {
-                System.out.println("Integer=" + integer);
-            }
-        }
-        System.out.println("</Done>");
-        //executor.executor.shutdown();
     }
 }
---
+++ java-concurrency/src/test/java/chapter_08/ExecutorConfigurationTest.java
@@ -0,0 +1,64 @@
+package chapter_08;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import util.StopWatch;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.*;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by vmc on 8/07/15.
+ */
+@RunWith(JUnit4.class)
+public class ExecutorConfigurationTest {
+    private ExecutorConfiguration executorConfiguration;
+
+    private int corePoolSize = 10;
+    private int maximumPoolSize = 10;
+    private int keepAliveTime = 2;
+
+    @Before
+    public void before() {
+        executorConfiguration = new ExecutorConfiguration(corePoolSize, maximumPoolSize, keepAliveTime, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+    }
+
+    @Test
+    public void shouldTest() throws InterruptedException {
+        List<Callable<Integer>> tasks = new LinkedList<Callable<Integer>>();
+        int numberOfCallable = 10;
+        int sleepingTime = 1000;
+        for (int i = 1; i <= numberOfCallable; i++) {
+            tasks.add(executorConfiguration.makeCallable(i, sleepingTime));
+        }
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        List<Future<Integer>> fs = executorConfiguration.executor.invokeAll(tasks);
+        stopWatch.stop();
+
+        assertTrue("main thread should be blocked until invokeAll finish", 5 <= stopWatch.toSecondValue());
+
+        int total = 0;
+        for (int i = 0; i < numberOfCallable; i++) {
+            Future<Integer> f = fs.get(i);
+            Integer integer = null;
+            try {
+                integer = f.get();
+                total += integer;
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println("Integer=" + integer);
+            }
+        }
+        assertThat(total, is(55));
+    }
+}
